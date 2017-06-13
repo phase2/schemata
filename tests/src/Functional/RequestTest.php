3c05b78e38b733a21675427011602a35c3b5bc4e
Ted Bowman <ted.git@tedbow.com>
2017-06-13 08:07:32 -0400

    Applied reroll changes https://www.drupal.org/node/2870904#comment-12125453

--- tests/src/Functional/RequestTest.php
+++ tests/src/Functional/RequestTest.php
@@ -2,15 +2,11 @@
 
 namespace Drupal\Tests\schemata\Functional;
 
-use Behat\Mink\Driver\BrowserKitDriver;
-use Drupal\Core\Url;
 use Drupal\field\Entity\FieldConfig;
 use Drupal\field\Entity\FieldStorageConfig;
 use Drupal\node\Entity\NodeType;
 use Drupal\taxonomy\Entity\Vocabulary;
 use Drupal\Tests\BrowserTestBase;
-use GuzzleHttp\RequestOptions;
-use Psr\Http\Message\ResponseInterface;
 
 /**
  * Tests requests schemata routes.
@@ -20,13 +16,26 @@ use Psr\Http\Message\ResponseInterface;
 class RequestTest extends BrowserTestBase {
 
   /**
+   * Set to TRUE to run this test to generate expectation files.
+   *
+   * The test will be marked as a fail when generating test files.
+   *
+   * @var bool
+   */
+  protected $generateExpectationFiles = FALSE;
+
+  /**
    * {@inheritdoc}
    */
   public static $modules = [
+    'user',
+    'field',
+    'filter',
     'text',
     'node',
     'taxonomy',
     'serialization',
+    'hal',
     'schemata',
     'schemata_json_schema',
   ];
@@ -81,107 +90,87 @@ class RequestTest extends BrowserTestBase {
   public function testRequests() {
     /** @var \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager */
     $entity_type_manager = $this->container->get('entity_type.manager');
-    $options = [
-      'query' => [
-        '_format' => 'schema_json',
-        '_describes' => 'json',
-      ],
-    ];
-    foreach ($entity_type_manager->getDefinitions() as $entity_type_id => $entity_type) {
 
-      $response = $this->request('GET', Url::fromRoute("schemata.$entity_type_id", [], $options), []);
-      $this->checkExpectedResponse($response, $entity_type_id);
-      if ($bundle_type = $entity_type->getBundleEntityType()) {
-        $bundles = $entity_type_manager->getStorage($bundle_type)->loadMultiple();
-        foreach ($bundles as $bundle) {
-          $response = $this->request('GET', Url::fromRoute("schemata.$entity_type_id:{$bundle->id()}", [], $options), []);
-          $this->checkExpectedResponse($response, $entity_type_id, $bundle->id());
+    foreach (['json', 'hal_json', 'api_json'] as $format) {
+      foreach ($entity_type_manager->getDefinitions() as $entity_type_id => $entity_type) {
+        $this->requestSchema($format, $entity_type_id);
+        if ($bundle_type = $entity_type->getBundleEntityType()) {
+          $bundles = $entity_type_manager->getStorage($bundle_type)->loadMultiple();
+          foreach ($bundles as $bundle) {
+            $this->requestSchema($format, $entity_type_id, $bundle->id());
+          }
         }
       }
     }
-  }
-
-  /**
-   * Performs a HTTP request. Wraps the Guzzle HTTP client.
-   *
-   * Why wrap the Guzzle HTTP client? Because we want to keep the actual test
-   * code as simple as possible, and hence not require them to specify the
-   * 'http_errors = FALSE' request option, nor do we want them to have to
-   * convert Drupal Url objects to strings.
-   *
-   * We also don't want to follow redirects automatically, to ensure these tests
-   * are able to detect when redirects are added or removed.
-   *
-   * @param string $method
-   *   HTTP method.
-   * @param \Drupal\Core\Url $url
-   *   URL to request.
-   * @param array $request_options
-   *   Request options to apply.
-   *
-   * @return \Psr\Http\Message\ResponseInterface
-   *   The response.
-   *
-   * @see \GuzzleHttp\ClientInterface::request()
-   */
-  protected function request($method, Url $url, array $request_options) {
-    $request_options[RequestOptions::HTTP_ERRORS] = FALSE;
-    $request_options[RequestOptions::ALLOW_REDIRECTS] = FALSE;
-    $request_options = $this->decorateWithCookies($request_options);
-    $client = $this->getSession()->getDriver()->getClient()->getClient();
-    return $client->request($method, $url->setAbsolute(TRUE)->toString(), $request_options);
-  }
-
-  /**
-   * Adds the cookies from the client to the request options.
-   *
-   * @param array $request_options
-   *   The request options.
-   *
-   * @return array
-   *   Request options updated with the client cookies if present.
-   */
-  protected function decorateWithCookies(array $request_options) {
-    $session = $this->getSession();
-    $driver = $session->getDriver();
-    if ($driver instanceof BrowserKitDriver) {
-      $client = $driver->getClient();
-      foreach ($client->getCookieJar()->all() as $cookie) {
-        if (isset($request_options[RequestOptions::HEADERS]['Cookie'])) {
-          $request_options[RequestOptions::HEADERS]['Cookie'] .= '; ' . $cookie->getName() . '=' . $cookie->getValue();
-        }
-        else {
-          $request_options[RequestOptions::HEADERS]['Cookie'] = $cookie->getName() . '=' . $cookie->getValue();
-        }
-      }
+    if ($this->generateExpectationFiles) {
+      $this->fail('Expectation fails generated. Tests not run.');
     }
-    return $request_options;
+
   }
 
   /**
-   * Check the expected response.
+   * Makes schema request and checks the response.
    *
-   * @param \Psr\Http\Message\ResponseInterface $response
-   *   The response from the test request.
+   * @param string $format
+   *   The described format.
    * @param string $entity_type_id
    *   Then entity type.
    * @param string|null $bundle_name
    *   The bundle name or NULL.
    */
-  protected function checkExpectedResponse(ResponseInterface $response, $entity_type_id, $bundle_name = NULL) {
-    $this->assertEquals('200', $response->getStatusCode());
+  protected function requestSchema($format, $entity_type_id, $bundle_name = NULL) {
+    $options = [
+      'query' => [
+        '_format' => 'schema_json',
+        '_describes' => $format,
+      ],
+    ];
+    $contents = $this->drupalGet("schemata/$entity_type_id/$bundle_name", $options);
+    $this->assertSession()->statusCodeEquals(200);
     if (in_array($entity_type_id, ['node', 'taxonomy_term'])) {
-      $contents = $response->getBody()->getContents();
+      $this->assertFalse(empty($contents), "Content not empty for $format, $entity_type_id, $bundle_name");
       $file_name = __DIR__ . "/../../expectations/";
       if ($bundle_name) {
-        $file_name .= "$entity_type_id.$bundle_name.json.json";
+        $file_name .= "$entity_type_id.$bundle_name.$format.json";
       }
       else {
-        $file_name .= "$entity_type_id.json.json";
+        $file_name .= "$entity_type_id.$format.json";
       }
-      $expected = file_get_contents($file_name);
-      $this->assertEquals($expected, $contents);
+
+      if ($this->generateExpectationFiles) {
+        $this->saveExpectationFile($file_name, $contents);
+        // Response assertion is not performed when generating expectation
+        // files.
+        return;
+      }
+
+      // Compare decoded json to so that failure will indicate which element is
+      // incorrect.
+      $expected = json_decode(file_get_contents($file_name), TRUE);
+      $expected['id'] = str_replace('{base_url}', $this->baseUrl, $expected['id']);
+      $decoded_response = json_decode($contents, TRUE);
+
+      $this->assertEquals($expected, $decoded_response, "The response matches expected file: $file_name");
     }
+  }
+
+  /**
+   * Saves an expectation file.
+   *
+   * @param string $file_name
+   *   The file name of the expectation file.
+   * @param string $contents
+   *   The JSON response contents.
+   *
+   * @see \Drupal\Tests\schemata\Functional\RequestTest::$generateExpectationFiles
+   */
+  protected function saveExpectationFile($file_name, $contents) {
+    $decoded_response = json_decode($contents, TRUE);
+    // Replace the base url because will be different for different
+    // environments.
+    $decoded_response['id'] = str_replace($this->baseUrl, '{base_url}', $decoded_response['id']);
+    $re_encode = json_encode($decoded_response, JSON_PRETTY_PRINT);
+    file_put_contents($file_name, $re_encode);
   }
 
 }
