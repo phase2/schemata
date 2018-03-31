9ca645496e9642e986cb59881d7f8938e2a3bede
Adam Ross <aross@phase2technology.com>
2018-03-31 16:52:39 -0700

    Undo addition of schema type changes to controller in this branch

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -7,10 +7,8 @@ use Drupal\Core\Cache\CacheableResponse;
 use Drupal\Core\Cache\CacheableResponseInterface;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\schemata\SchemaFactory;
-use Drupal\schemata\SchemaTypeManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Serializer\Exception\UnexpectedValueException;
 use Symfony\Component\Serializer\SerializerInterface;
 
 /**
@@ -33,13 +31,6 @@ class Controller extends ControllerBase {
   protected $schemaFactory;
 
   /**
-   * SchemaTypeManager.
-   *
-   * @var \Drupal\schemata\SchemaTypeManager
-   */
-  protected $typeManager;
-
-  /**
    * The cacheable response.
    *
    * @var \Drupal\Core\Cache\CacheableResponseInterface
@@ -53,15 +44,12 @@ class Controller extends ControllerBase {
    *   The serializer service.
    * @param \Drupal\schemata\SchemaFactory $schema_factory
    *   The schema factory.
-   * @param \Drupal\schemata\SchemaTypeManager $type_manager
-   *   SchemaTypeManager.
    * @param \Drupal\Core\Cache\CacheableResponseInterface $response
    *   The cacheable response.
    */
-  public function __construct(SerializerInterface $serializer, SchemaFactory $schema_factory, SchemaTypeManager $type_manager, CacheableResponseInterface $response) {
+  public function __construct(SerializerInterface $serializer, SchemaFactory $schema_factory, CacheableResponseInterface $response) {
     $this->serializer = $serializer;
     $this->schemaFactory = $schema_factory;
-    $this->typeManager = $type_manager;
     $this->response = $response;
   }
 
@@ -72,7 +60,6 @@ class Controller extends ControllerBase {
     return new static(
       $container->get('serializer'),
       $container->get('schemata.schema_factory'),
-      $container->get('schemata.type_manager'),
       new CacheableResponse()
     );
   }
@@ -94,65 +81,16 @@ class Controller extends ControllerBase {
    *
    * @return \Drupal\Core\Cache\CacheableResponse
    *   The response object.
-   *
-   * @todo Handle an empty _format query parameter. https://www.drupal.org/node/2773009
-   * @todo Handle an empty _describes query parameter. https://www.drupal.org/node/2910018
-   * @todo Handle MIME type not associated with the given schema type.
    */
   public function serialize($entity_type_id, Request $request, $bundle = NULL) {
-    list ($schema_type, $described_format) = $this->extractFormatNames($request);
+    $parts = $this->extractFormatNames($request);
 
-    // Load the schema data to serialize from the request route information.
-    /** @var \Drupal\schemata\Schema\SchemaInterface $schema */
+    // Load the data to serialize from the route information on the current
+    // request.
     $schema = $this->schemaFactory->create($entity_type_id, $bundle);
-    $format = "{$schema_type}:{$described_format}";
-    $mime_type = $request->getMimeType($schema_type);
-
-    try {
-      if (!$this->typeManager->schemaTypeExists($schema_type)) {
-        $error = [
-          'title' => t('Unrecognized Schema Type'),
-          'details' => t('Your requested schema type format @type is not supported.', [
-            '@type' => $schema_type
-          ]),
-          'status' => 400,
-          'instance' => "#{$schema_type}",
-        ];
-        $mime_type = 'application/problem+json';
-        $content = $this->serializer->serialize($error, 'json');
-        $this->response->setStatusCode($error['status']);
-      }
-      elseif (!$this->typeManager->schemaTypeSupportsFormat($schema_type, $described_format)) {
-        $error = [
-          'title' => t('Unrecognized Format for Schema Description'),
-          'details' => t('The format you requested to be described by @type is not available for @described.', [
-            '@type' => $schema_type,
-            '@described' => $described_format,
-          ]),
-          'status' => 400,
-          'instance' => "#{$schema_type}-{$described_format}",
-        ];
-        $mime_type = 'application/problem+json';
-        $content = $this->serializer->serialize($error, 'json');
-        $this->response->setStatusCode($error['status']);
-      }
-      else {
-        // Serialize the entity type/bundle definition.
-        $content = $this->serializer->serialize($schema, $format);
-      }
-    }
-    catch(\Exception $e) {
-      $error = [
-        'title' => t('Error Assembling Schema'),
-        'details' => t('Something went wrong building your schema after collecting the data and during the process of arrranging it for your selected schema format.'),
-        'status' => 500,
-        // @todo Log this error instead of returning it in the message.
-        'error' => $e->getMessage(),
-      ];
-      $mime_type = 'application/problem+json';
-      $content = $this->serializer->serialize($error, 'json');
-      $this->response->setStatusCode($error['status']);
-    }
+    // Serialize the entity type/bundle definition.
+    $format = implode(':', $parts);
+    $content = $this->serializer->serialize($schema, $format);
 
     // Finally, set the contents of the response and return it.
     $this->response->addCacheableDependency($schema);
@@ -160,8 +98,7 @@ class Controller extends ControllerBase {
       ->addCacheContexts(['url.query_args:_describes']);
     $this->response->addCacheableDependency($cacheable_dependency);
     $this->response->setContent($content);
-    $this->response->headers->set('Content-Type', $mime_type);
-
+    $this->response->headers->set('Content-Type', $request->getMimeType($parts[0]));
     return $this->response;
   }
 
